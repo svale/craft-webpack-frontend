81106acbcd7add236a7968210056e7faaaf4ec8d
Svale Fossåskaret <svale@3by5.no>
2021-03-01 16:31:33 +0100

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+// tailwind.config.js
+// See more information about this file at https://tailwindcss.com/docs/installation#create-your-configuration-file
+
+module.exports = {
+  purge: {
+    enabled: false, // Turn on to purge also in dev
+    content: [
+      './src/**/*.js',
+      './templates/**/*.twig',
+      './templates/**/*.html'
+    ],
+  },
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {},
+  plugins: [],
+};
